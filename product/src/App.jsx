ceae5ad213db82ae4233b184459eead9a07cde84
phuhoang <hoangvanphu2006@gmail.com>
2025-04-21 09:50:41 +0700

    form login

--- product/src/App.jsx
+++ product/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import RoutersAdmin from "./router/admin/RoutersAdmin.jsx";
+import RoutersLogin from "./router/login/RoutersLogin.jsx";
 function App() {
   return (
       <>
+          <RoutersLogin/>
           <RoutersAdmin/>
       </>
   )
